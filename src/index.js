15f229313ac1c51940e7ee661d3d15de583fd43b
Matt Mankins <mankins@gmail.com>
2021-05-14 13:49:35 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -1,107 +1,121 @@
-const express = require("express");
+const marked = require('marked');
+const fs = require('fs');
+const express = require('express');
 const app = express();
-app.use(express.static("static"));
+app.use(express.static('static'));
+app.use('/static', express.static('static')); // github readme compat
 app.use(express.json());
 
-const { parse, parseValue } = require("parse-accepts");
-
+const { parse, parseValue } = require('parse-accepts');
 
 const PORT = process.env.PORT || 3000;
 
-app.all("/headers", async (req, res) => {
-  const { headers } = req;
-  res.send({headers});
+app.get('/', (req, res) => {
+  const index = fs.readFileSync('./readme.md').toString();
+  const html = marked(index);
+  res.send(page(html));
 });
 
-app.all("/accept-payment", async (req, res) => {
+app.all('/headers', async (req, res) => {
+  const { headers } = req;
+  res.send({ headers });
+});
+
+app.all('/accept-payment', async (req, res) => {
   const { headers } = req;
   const acceptPayment = headers['accept-payment'] || '';
 
-    if (acceptPayment) {
-  const parsed = parse(acceptPayment);
-res.send({parsed});
-return;
-    } else {
-      res.send(`<h1>Missing Payment Header</h1>
+  if (acceptPayment) {
+    const parsed = parse(acceptPayment);
+    res.send({ parsed });
+    return;
+  } else {
+    res.send(`<h1>Missing Payment Header</h1>
       
       <a href="/headers">Inspect headers</a>
       `);
+  }
+});
 
+app.all('/ads/required', async (req, res) => {
+  const { headers } = req;
+  const acceptPayment = headers['accept-payment'] || '';
+
+  const lookingFor = 'ads';
+
+  const parsed = parse(acceptPayment) || [];
+  let found = false;
+  parsed.forEach((type) => {
+    if (type.value && type.value.indexOf(lookingFor) !== -1) {
+      if (type.quality) {
+        found = true;
+      }
     }
+  });
+  if (found) {
+    res.send(page('Ok, method found'));
+    return;
+  }
+
+  res.status(402);
+  res.send(
+    page(
+      '<h1>Missing Payment: 402 Payment Required</h1><p>Accept-payment: ads/* not found</p>',
+    ),
+  );
 });
 
-
-app.all("/crazypass/required", async (req, res) => {
+app.all('/webmon/required', async (req, res) => {
   const { headers } = req;
   const acceptPayment = headers['accept-payment'] || '';
 
-   const lookingFor = 'crazypass';
+  const lookingFor = 'webmon';
 
   const parsed = parse(acceptPayment) || [];
   let found = false;
   parsed.forEach((type) => {
-      if (type.value && type.value.indexOf(lookingFor) !== -1) {
-          if (type.quality) {
-          found = true;
-          }
+    if (type.value && type.value.indexOf(lookingFor) !== -1) {
+      if (type.quality) {
+        found = true;
       }
+    }
   });
   if (found) {
-res.send('Ok, method found');
-return;
+    res.send(page('Ok, method found'));
+    return;
   }
 
-    fourOhTwo(req, res);  
+  res.status(402);
+  res.send(
+    page(
+      '<h1>Missing Payment: 402 Payment Required</h1><p>Accept-payment: webmon/* not found</p>',
+    ),
+  );
 });
 
-app.all("/webmon/required", async (req, res) => {
-  const { headers } = req;
-  const acceptPayment = headers['accept-payment'] || '';
+const page = (body) => {
+  return `${body}
+    <h2>Pages</h2>
+          <ul>
+       <li><a href="/headers">Inspect headers</a></li>
+       <li><a href="/accept-payment">Parse Accept-Payment</a></li>
+       <li><a href="/webmon/required">Look for Web Monetization</a></li>
+       <li><a href="/ads/required">Look for Ads</a></li>
+       <li><a href="/">Overview</a></li>
+      </ul>`;
+};
 
-   const lookingFor = 'webmon';
-
-  const parsed = parse(acceptPayment) || [];
-  let found = false;
-  parsed.forEach((type) => {
-      if (type.value && type.value.indexOf(lookingFor) !== -1) {
-          if (type.quality) {
-          found = true;
-          }
-      }
-  });
-  if (found) {
-res.send('Ok, method found');
-return;
-  }
-
-    fourOhTwo(req, res);  
-});
-
-
-app.all("/", async (req, res) => {
+app.all('/', (req, res) => {
   const { headers } = req;
 
   if (headers['accept-payment']) {
-      res.send(`<h1>Payment Accepted</h1>
-      
-      <a href="/headers">Inspect headers</a>
-      <a href="/accept-payment">Parse Accept-Payment</a>
-      `);
-      return;
+    res.send(page('<h1>Payment Accept Header Found. Ok.</h1>'));
   } else {
-      await fourOhTwo(req, res);
+    res.status(402);
+    res.send(page('<h1>Missing Payment: 402 Payment Required</h1>'));
   }
 });
 
-const fourOhTwo = (req, res) => {
-      res.status(402);
-      res.send(`<h1>Missing Payment: 402 Payment Required</h1>
-      
-      <a href="/headers">Inspect headers</a>
-      <a href="/accept-payment">Parse Accept-Payment</a>
-      `);
-};
-
- app.listen(PORT, () => {
-     console.log(`accept-payment on ${PORT}`);
- });
+app.listen(PORT, () => {
+  console.log(`accept-payment on http://localhost:${PORT}/`);
+});
